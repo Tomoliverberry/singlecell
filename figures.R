6f089179b96dc4c7a19195752a7d909cb1a64d3a
Dazcam <camerond@cardiff.ac.uk>
2021-07-21 15:46:19 +0100

    Added code to create pre-filter DF

--- figures.R
+++ figures.R
@@ -10,3 +10,33 @@ df[1 %in% `510-Pre-Filter-Metadata`$Keep, ]
 `510-Pre-Filter-Metadata`["Keep" ==1]
 which(`510-Pre-Filter-Metadata`["Keep"] == 1)
 `510-Pre-Filter-Metadata`(Keep)[names(`510-Pre-Filter-Metadata`(Keep)) == 1]
+
+## Solution - note that I removed line 53 from the code sent in the email as it was superfluous
+
+##  Inital ArchR QC -------------------------------------------------------------------
+# ArchR does some QC when loading the files in so need to load the pre-QC info
+# Pre-filter
+for (SAMPLE in 1:length(SAMPLES)) {
+  
+  # Subset IDs
+  sampleID <- substr(SAMPLES[SAMPLE], 1, 7)
+  donorID <- substr(SAMPLES[SAMPLE], 1, 3)
+  
+  # Load Pre-filtered data
+  preQC_df <- readRDS(paste0(WK_DIR, "QualityControl/", SAMPLES[SAMPLE], "/", 
+                             SAMPLES[SAMPLE], "-Pre-Filter-Metadata.rds"))
+  
+  # Assign frag plots and counts_df
+  assign(paste0("counts_df_", donorID), 
+         data.frame("Sample" = sampleID,
+                    "Cells_Pass_Filter" = sum(preQC_df$Keep),
+                    "Cells_dropped" = sum(preQC_df$Keep == 0),
+                    "Total_Frags" = sum(preQC_df$nFrags),
+                    "Median_Frags" = median(preQC_df$nFrags[preQC_df$Keep == 1]),
+                    "Median_TSS_Enrichment" = median(preQC_df$TSSEnrichment[preQC_df$Keep == 1])))
+  
+}
+
+## Initial QC reporting  --------------------------------------------------------------
+# Counts df
+counts_df <- rbind(counts_df_510, counts_df_611, counts_df_993)
